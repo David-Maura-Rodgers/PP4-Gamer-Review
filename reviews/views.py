e8d1facf3377bd627f68d8839ac3678232620c73
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-15 22:11:39 +0000

    add: base.html and import google fonts

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+class ReviewList(generic.ListView):
+    model = Review
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
