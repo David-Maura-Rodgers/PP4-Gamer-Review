edd758066c93ab0e2b01b699ecb0b2e5c9a85ef7
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-15 23:46:00 +0000

    add: css styling and class names

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Review
+
 
 class ReviewList(generic.ListView):
     model = Review
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Review.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class ReviewDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "review": review,
+                "comments": comments,
+                "liked": liked
+            },
+        )
