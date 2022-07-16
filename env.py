2268b55f7cc221af511ba132e926d1fce31bb450
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-16 20:09:23 +0000

    add: new colours, text size and image for review_detail.html

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://extkzufkkdodfy:89831674a76464a530ea54af3d5854fa7c9e8fb4e9c4ee7db197866d9f23dbdd@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dejtuhea0ujv4n"
+os.environ["SECRET_KEY"] = "randomDavidKey1984!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://182122622665892:-xlz5mon7gXauSIrPwJPaIPVdho@davidr-code-institute"
