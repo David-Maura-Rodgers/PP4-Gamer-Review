27646db5694be0908027c33627a1c78027e39bc2
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-17 21:58:18 +0000

    add: create test to check that home page renders correctly

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """
+    Test home page for review app
+    """
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
