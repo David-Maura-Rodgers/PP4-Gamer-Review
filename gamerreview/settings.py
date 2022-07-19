0526fd448b8ca223ca9e0128b1ceec5f2feb963d
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-19 21:10:14 +0000

    add: install crispy-forms and add this to settings Installed Apps

--- gamerreview/settings.py
+++ gamerreview/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
