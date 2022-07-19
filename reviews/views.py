6de46e2494f07b0a4982e64c951bdc992a595006
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-19 21:36:17 +0000

    add: forms.py and post function to render comment box in review_detail.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Review
+from .forms import CommentForm
 
 
 class ReviewList(generic.ListView):
@@ -26,6 +27,39 @@ class ReviewDetail(View):
             {
                 "review": review,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.review = review
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "review": review,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
                 "liked": liked
             },
         )
