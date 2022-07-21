508fd4c80c17d55c3ffc35cc55b38e89b872e52f
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-21 20:41:59 +0000

    add: rough draft for GamerReviw model

--- create/models.py
+++ create/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class GamerReview(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    gamer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="create_review"
+    )
+    game = models.TextField(default='placeholder')
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
--- create/views.py
+++ create/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Create
+from .forms import CommentForm
 
-# Create your views here.
+
+class CreateView(generic.ListView):
+    model = Create
+    queryset = Create.objects.filter(status=1).order_by('-created_on')
+    template_name = 'create.html'
+    paginate_by = 6
