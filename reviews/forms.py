8200b4c98c6659cb51d738143b186ed35a10a2a5
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-08-02 17:53:33 +0000

    add: summernote on front end for review forms so user can select what font and stle they want

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,6 @@
 from .models import Comment, Review
 from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 
 class CommentForm(forms.ModelForm):
@@ -9,6 +10,11 @@ class CommentForm(forms.ModelForm):
 
 
 class ReviewForm(forms.ModelForm):
+    '''
+    Allows user to enter data in the fields below
+    '''
+    content = forms.CharField(widget=SummernoteWidget())
+
     class Meta:
         model = Review
         fields = ('title', 'game', 'subtitle', 'content',)
