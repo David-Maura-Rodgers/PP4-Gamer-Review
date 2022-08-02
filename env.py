a804549d10e6f95f93639befa666dc57f72af4bf
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-08-02 14:24:10 +0000

    erased env.py file to correct gitignore requirement

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://jyfbirpbngfuml:4773ca202d895427563e7a54b5b203d71c2acadbc9838e23cc4d7a9250f7990a@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d404459bvk8e5v"
-os.environ["SECRET_KEY"] = "randomDavidKey1984!!"
-os.environ["CLOUDINARY_URL"] = "cloudinary://182122622665892:-xlz5mon7gXauSIrPwJPaIPVdho@davidr-code-institute"
